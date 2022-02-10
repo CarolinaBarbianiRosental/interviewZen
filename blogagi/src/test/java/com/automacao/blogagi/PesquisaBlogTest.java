2058b6659dc5e1b44ab2a8830fe6cd36d53067ec
Carolina <barbianirosental@gmail.com>
2022-02-10 00:11:15 -0300

    Correção do nome do arquivo

--- blogagi/src/test/java/com/automacao/blogagi/PesquisaBlogTest.java
+++ blogagi/src/test/java/com/automacao/blogagi/PesquisaBlogTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class PesqusaBlogTest {
+public class PesquisaBlogTest {
 
 	public WebDriver driver;
 	public HomeBlog homeblog;
