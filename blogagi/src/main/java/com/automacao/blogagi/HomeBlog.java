7f33acf8087108fd7cf5ec1ebd7eb6da6a9e195f
Carolina <barbianirosental@gmail.com>
2022-02-10 00:05:35 -0300

    Adicionando casos teste
    Adicionando um caso de teste positivo e um negativo.

--- blogagi/src/main/java/com/automacao/blogagi/HomeBlog.java
+++ blogagi/src/main/java/com/automacao/blogagi/HomeBlog.java
@@ -2,9 +2,10 @@ package com.automacao.blogagi;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HomeBlog {
-	
+
 	private WebDriver driver;
 
 	public HomeBlog(WebDriver driver) {
@@ -15,7 +16,24 @@ public class HomeBlog {
 		driver.get("https://blogdoagi.com.br/");
 		return this;
 	}
-	
-	
 
+	public void clicarLupa() {
+		driver.findElement(By.id("search-open")).click();
+	}
+
+	public void inserirTexto(String texto) {
+		WebElement element = driver.findElement(By.className("search-field"));
+		element.sendKeys(texto);
+		element.submit();
+	}
+
+	public String obtemResultadoValido() {
+			return driver.findElement(By.className("archive-title")).getText();
+	}
+	
+	public String obtemResultadoVazio() {
+		WebElement element = driver.findElement(By.className("no-results"));
+		return element.findElement(By.className("entry-header")).getText();
+		
+}
 }
--- blogagi/src/test/java/com/automacao/blogagi/PesqusaBlogTest.java
+++ blogagi/src/test/java/com/automacao/blogagi/PesqusaBlogTest.java
@@ -3,38 +3,56 @@ package com.automacao.blogagi;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-
 public class PesqusaBlogTest {
-	
-	   public WebDriver driver;
-	   public HomeBlog homeblog;
-	
-	 @Before
-	    public void abrirNavegador() {
-	        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Windows\\chromedriver_win32\\chromedrive\\chromedriver.exe");
-	        ChromeOptions options = new ChromeOptions();
-	        options.addArguments("--start-maximized");
-	        driver = new ChromeDriver(options);
-	        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-	    }
 
-	    @After
-	    public void fecharNavegador() {
-	        driver.quit();
-	    }
+	public WebDriver driver;
+	public HomeBlog homeblog;
 
-	    @Test
-	   public void pesquisaArtigoValido() {
-	    	 homeblog = new HomeBlog(driver);
-	    	 
-	    	 homeblog.abrirSite();
-	    	  
-	    }
-	
+	@Before
+	public void abrirNavegador() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Windows\\chromedriver_win32\\chromedrive\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start-maximized");
+		driver = new ChromeDriver(options);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+	}
+
+	@After
+	public void fecharNavegador() {
+		driver.quit();
+	}
+
+	@Test
+	public void pesquisaAssuntoValido() {
+		homeblog = new HomeBlog(driver);
+
+		homeblog.abrirSite();
+		homeblog.clicarLupa();
+		homeblog.inserirTexto("Fabrica");
+		String resultado = homeblog.obtemResultadoValido();
+		System.out.println(resultado);
+		Assert.assertEquals(resultado, "Resultados da busca por: Fabrica");
+	}
+
+	@Test
+	public void pesquisaAssuntoNaoEncontrado(){
+		homeblog = new HomeBlog(driver);
+
+		homeblog.abrirSite();
+		homeblog.clicarLupa();
+		homeblog.inserirTexto("Testes Automatizados");
+		String resultado = homeblog.obtemResultadoVazio();
+		System.out.println(resultado);
+		Assert.assertEquals(resultado, "Nenhum resultado");
+
+	}
+
 }
